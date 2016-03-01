62b7b88ac8a92471310051d18f27b4fa3a547c7c
Mr CTG <chithanh12@gmail.com>
2016-03-01 14:16:41 +0700

    Init all init script

---
+++ src/init_data.sh
@@ -0,0 +1,1 @@
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin@123')" | ./manage.py shell
---
+++ src/vendor_container_init.sh
@@ -0,0 +1,2 @@
+pip install -r requirements.txt --user --upgrade
+$PYTHONUSERBASE/bin/django-admin startproject $PROJECTNAME .
